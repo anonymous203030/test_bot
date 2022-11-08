5878840aaa3d98cab68741953af23f88a13aa4df
anonymous203030 <aram.simonyan.03@gmail.com>
2022-11-08 22:19:43 +0400

    configurations file

---
+++ configs.py
@@ -0,0 +1,10 @@
+API_HASH: str = '5851d713142f82d15774a5cfa11210e4'
+API_ID: int = 1896955
+BOT_TOKEN: str = '5248959624:AAGYc-V8_4Dz-dT5Ze1sYsYsLfocikGHiiI'
+UPWORK_XML_URL: str = 'https://www.upwork.com/ab/feed/jobs/rss?sort=recency&category2_uid=531770282580668421&co' \
+                      'ntractor_tier=3&proposals=0-4&verified_payment_only=1&paging=0%3B10&api_params=1&q=' \
+                      '&securityToken=2c2762298fe1b719a51741dbacb7d4f5c1e42965918fbea8d2bf1185644c8ab2907f41' \
+                      '8fe6b1763d5fca3a9f0e7b34d2047f95b56d12e525bc4ba998ae63f0ff&userUid=424312217100599296&orgUi' \
+                      'd=424312217104793601&ptc=1229907576325861376%2C1096176094801088512%2C148171030' \
+                      '1906190336%2C1431305224146247680'
+CHAT_ID: str = 'https://t.me/test_projec'
